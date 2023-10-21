8bfe173c9965177151423e03e9298aa1b7a17cec
Visal Korng <visalkorng8888@gmail.com>
2023-10-21 15:43:04 +0700

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <ul>
           <li>
-            <Link to="/">To Visalll</Link>
+            <Link to="/">To Visall</Link>
           </li>
           <li>
             <Link to="/profile">To Profile</Link>
